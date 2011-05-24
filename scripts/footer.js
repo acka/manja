576f81aa08bc089f871c6aae19993b3344c27971
Sasa <acka.milenkovic@gmail.com>
2011-05-24 12:56:22 +0200

    resized footer images to 100px, added new images

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,7 @@
 
 $('footer').addClass(function(i, c) {
 		var rndClass = "ft_";
-		var rnd = 1 + Math.floor(Math.random() * 14);
+		var rnd = 1 + Math.floor(Math.random() * 24);
 		if (rnd < 10) {
 			rndClass += "0";
 		}
